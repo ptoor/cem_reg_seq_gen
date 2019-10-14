ee1b09e877c6762a12ec4d6dd5bd7d97827561e2
Pranav Toor <pranav.toor@yahoo.com>
2019-10-14 16:54:24 +0200

    updated to take lowercase

--- regseq_gen.py
+++ regseq_gen.py
@@ -3,7 +3,7 @@ import argparse
 def get_reglist(file_handle):
     reglist = []
     for line in file_handle:
-        reglist.append(line.split(' ')[0])
+        reglist.append(line.split(' ')[0].lower())
     return(reglist)
 
 regs = get_reglist(open(sys.argv[1], "r"))
