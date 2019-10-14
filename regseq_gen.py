70c6df1afe70c0f8bcf986ae2d3634be0bf378d3
ptoor <36647233+ptoor@users.noreply.github.com>
2019-10-14 13:33:34 +0200

    first version

---
+++ regseq_gen.py
@@ -0,0 +1,10 @@
+import sys
+import argparse
+def get_reglist(file_handle):
+    reglist = []
+    for line in file_handle:
+        reglist.append(line.split(' ')[0])
+    return(reglist)
+
+regs = get_reglist(open(sys.argv[1], "r"))
+print(regs)
